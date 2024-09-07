dd933dab5d178ff1e1b4818dc20d2248718edb92
Kavindu Dissanayake <51842810+KavinduDissanayake@users.noreply.github.com>
2024-09-07 22:38:49 +0530

    change accessbility

--- Sources/KDNativeShimmer/AnimatePlaceholderModifier.swift
+++ Sources/KDNativeShimmer/AnimatePlaceholderModifier.swift
@@ -5,69 +5,43 @@
 //  Created by KavinduDissanayake on 2024-09-07.
 //  All rights reserved for KDmobileApps.
 //
-
 import SwiftUI
-import UIKit 
 
 // MARK: - AnimatePlaceholderModifier
 
 /// Modifier to apply a shimmering effect to views while loading.
 public struct AnimatePlaceholderModifier: AnimatableModifier {
     
-    @Binding private var isLoading: Bool
+    @Binding var isLoading: Bool
     @State private var isAnim: Bool = false
-    private var center: CGFloat {
-        #if os(iOS)
-        return (UIScreen.main.bounds.width / 2) + 110
-        #else
-        return 110  // Provide a fallback for other platforms
-        #endif
-    }
+    private var center = (UIScreen.main.bounds.width / 2) + 110
     private let animation: Animation = .linear(duration: 1.5)
-    
-    // Public properties to allow customization
+
+    // Customizable colors and opacity
     public var shimmerBaseColor: Color
     public var shimmerHighlightColor: Color
     public var shimmerOpacity: Double
-    
-    // Public initializer for the modifier with configurable options
-    public init(
-        isLoading: Binding<Bool>,
-        shimmerBaseColor: Color = .gray,
-        shimmerHighlightColor: Color = .white.opacity(0.48),
-        shimmerOpacity: Double = 0.09
-    ) {
+
+    // Custom initializer
+    public init(isLoading: Binding<Bool>, shimmerBaseColor: Color = Color.gray, shimmerHighlightColor: Color = Color.white.opacity(0.48), shimmerOpacity: Double = 0.09) {
         self._isLoading = isLoading
         self.shimmerBaseColor = shimmerBaseColor
         self.shimmerHighlightColor = shimmerHighlightColor
         self.shimmerOpacity = shimmerOpacity
     }
 
-    // The body of the modifier.
     public func body(content: Content) -> some View {
-        content.overlay(isLoading ? animView.mask(content) : nil)
-            .onAppear {
-                guard isLoading else { return }
-                isAnim.toggle()
-            }
-            .onReceive([isLoading].publisher.first()) { _ in
-                isAnim.toggle()
-            }
+        content.overlay(animView.mask(content))
     }
 
-    // This is the internal view used to animate the shimmer effect.
+    // Shimmer view with custom colors
     private var animView: some View {
         ZStack {
-            // Use the configurable color and opacity
             shimmerBaseColor.opacity(isLoading ? shimmerOpacity : 0.0)
             Color.white.mask(
                 Rectangle()
                     .fill(
-                        LinearGradient(
-                            gradient: Gradient(colors: [.clear, shimmerHighlightColor, .clear]),
-                            startPoint: .top,
-                            endPoint: .bottom
-                        )
+                        LinearGradient(gradient: Gradient(colors: [.clear, shimmerHighlightColor, .clear]), startPoint: .top, endPoint: .bottom)
                     )
                     .scaleEffect(1.5)
                     .rotationEffect(.init(degrees: 70.0))
@@ -75,25 +49,20 @@ public struct AnimatePlaceholderModifier: AnimatableModifier {
             )
         }
         .animation(isLoading ? animation.repeatForever(autoreverses: false) : nil, value: isAnim)
+        .onAppear {
+            guard isLoading else { return }
+            isAnim.toggle()
+        }
+        .onChange(of: isLoading) { _ in
+            isAnim.toggle()
+        }
     }
 }
 
 // MARK: - View Extension
 
-/// A View extension to easily apply the shimmer effect.
 public extension View {
-    func animatePlaceholder(
-        isLoading: Binding<Bool>,
-        shimmerBaseColor: Color = .gray,
-        shimmerHighlightColor: Color = .white.opacity(0.48),
-        shimmerOpacity: Double = 0.09
-    ) -> some View {
-        self.modifier(AnimatePlaceholderModifier(
-            isLoading: isLoading,
-            shimmerBaseColor: shimmerBaseColor,
-            shimmerHighlightColor: shimmerHighlightColor,
-            shimmerOpacity: shimmerOpacity
-        ))
-        .redacted(reason: isLoading.wrappedValue ? .placeholderCircle : nil)
+    func animatePlaceholder(isLoading: Binding<Bool>, shimmerBaseColor: Color = Color.gray, shimmerHighlightColor: Color = Color.white.opacity(0.48), shimmerOpacity: Double = 0.09) -> some View {
+        self.modifier(AnimatePlaceholderModifier(isLoading: isLoading, shimmerBaseColor: shimmerBaseColor, shimmerHighlightColor: shimmerHighlightColor, shimmerOpacity: shimmerOpacity))
     }
 }
